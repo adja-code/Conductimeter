4a7f39a75be5ffc929ec5a4ac33d01c2a87f9529
dfed <dfed@dfed-ipgp5>
2025-05-25 19:32:15 +0200

    Ajout d'un fichier lib_conductimetre reprenant les fonctions  du pH-mètre afin de  les adapter aux conductimètres

---
+++ src/lib_conductimetre.py
@@ -0,0 +1,14 @@
+import serial
+import serial.tools.list_ports
+import os
+import glob
+import csv
+import time
+from datetime import datetime
+import numpy as np
+from scipy import interpolate
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+
+def port
