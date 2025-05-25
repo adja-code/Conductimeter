ac33fdd9a6be0d42024d423060142e53d34012a8
dfed <dfed@dfed-ipgp5>
2025-05-25 19:37:22 +0200

    Ajout de la fonction port_connexion qui permet d'établir la connexion au port série en fonction du numéro de série de l'arduino

--- src/lib_conductimetre.py
+++ src/lib_conductimetre.py
@@ -11,4 +11,60 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
 
-def port
+###############################################
+#
+# ACCES ARDUINO
+#
+###############################################
+
+def port_connexion(br = 9600 , portIN = '') :
+    """
+    Établit la connexion au port série.
+
+    Parameters
+    ----------
+    br : int
+        Flux de données en baud.
+    portIN : string
+        Identifiant du port série sur lequel le script doit lire des données.
+
+    Returns
+    -------
+    port : string
+        Identifiant du port série sur lequel le script doit lire des données.
+    s : serial.tools.list_ports_common.ListPortInfo / string
+        Objet Serial sur lequel on peut appliquer des fonctions d'ouverture, de lecture et de fermeture du port série affilié. En cas d'échec de connexion, 's' sera une chaîne de caractères "erreur".
+
+    """
+
+    #arduino_list=['85035323234351504260','85035323234351E09062','75439313737351402252','8503532323435130F142','75330303934351B05162']
+#remplacer par la liste des numéros de série des cartes arduinos des conductimètres 
+    if portIN == '' :
+        ports = list(serial.tools.list_ports.comports())
+    else :
+        ports = [portIN]  
+    i = 0 
+    conn = False
+    while conn == False :
+        try :
+            port = ports[i] 
+            if portIN == '' and (port.manufacturer == 'Arduino (www.arduino.cc)' or port.serial_number in arduino_list ):
+                port = port.device
+                port = (port).replace('cu','tty')
+                s = serial.Serial(port=port, baudrate=br, timeout=5) 
+                conn = True  
+                print('Connexion établie avec le port', port)
+            else :
+                s = serial.Serial(port=port, baudrate=br, timeout=5)
+                conn = True
+                print('Connexion établie avec le port', port)
+        except :
+            i += 1
+            if i >= len(ports) :
+                print("/!\ Port de connexion non détecté. Merci de rétablir la connexion non établie : connexion au processeur dans les réglages avant utilisation.")
+                s = 'error' 
+                portIN = '' 
+                conn = True
+            pass     
+    return port , s
+
