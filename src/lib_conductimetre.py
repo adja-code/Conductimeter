0851f68cbb647a5e31086de6d5ffa8dd6f5ad390
dfed <dfed@dfed-ipgp5>
2025-05-26 16:05:47 +0200

    Ajout de la fonction etalonnage dans le code python main

--- src/lib_conductimetre.py
+++ src/lib_conductimetre.py
@@ -68,7 +68,6 @@ def port_connexion(br = 9600 , portIN = '') :
             pass     
     return port , s
     
-###################################
     
     
 def fn_settings(portIN , s , br , nb_inter , time_inter) :
@@ -134,5 +133,100 @@ def fn_settings(portIN , s , br , nb_inter , time_inter) :
     else :
         print('/!\ Saisie invalide.')
     return portIN , s, br , nb_inter , time_inter
+ 
+#########################################################
+#
+# FONCTIONS DE CALIBRATION 
+#
+#########################################################
+
+def Etalonnage(nbr_etalon, nbr_mesure_par_etalon):
+    '''
+    Fonction qui, pour le nombre d'étalon indiqué en argument, mesure la tension, trouve la corrélation entre Tensino et conductivité, puis trace le graphique et renvoie la droite d'étalonnage
+
+    Parameters
+    ----------
+    nbr_etalon : int
+        Nombre d'étalon pour l'étalonnage.
+    nbr_mesure_par_etalon : int
+        Nombre de mesure à faire pour chaque étalon.
+
+    Returns
+    -------
+    droite : numpy.poly1d
+        Droite d'étalonnage sous la forme ax + b.
+
+    '''
+    conductimeter = setup()
+    list_tension = []
+    list_conductivite=[]
+    
+    #Correction de la conductivité par la température 
+    Temperature = [0,5,10,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+    Conductivite_12880 = [7150,8220,9330,10480,10720,10950,11190,11430,11670,11910,12150,12390,12640,12880,13130,13370,13620,13870,14120,14370]
+    Conductivite_1413 = [776,896,1020,1147,1173,1199,1225,1251,1278,1305,1332,1359,1386,1413,1440,1467,1494,1521,1548,1575]
+    Conductivite_5000 = [2760,3180,3615,4063,4155,4245,4337,4429,4523,4617,4711,4805,4902,5000,5096,5190,5286,5383,5479,5575]
+
+    reg_12880 = np.polyfit(Temperature,Conductivite_12880,1)
+    droite_12880 = np.poly1d(reg_12880)
+
+    reg_1413= np.polyfit(Temperature,Conductivite_1413,1)
+    droite_1413 = np.poly1d(reg_1413)
+
+    reg_5000= np.polyfit(Temperature,Conductivite_5000,1)
+    droite_5000 = np.poly1d(reg_5000)
+    
+    for k in range(nbr_etalon):
+        print('\n[ Étalon', k+1,']')
+        
+        conductivite = float(input('Quelle est la valeur de conductivité de votre étalon à 25°C ? (en uS/cm): '))
+        if conductivite == 12880 :
+            print('Remarque : Votre mesure sera corrigée avec la température mesurée dans l\'échantillon pour plus de précision.')
+            input('Appuyez sur Entrée après avoir plongé le thermomètre dans la solution.')
+            lect = conductimeter.readline().decode().strip('\r\n').split(',')
+            T = float(lect[0])
+            conductivite = droite_12880(T)
+        elif conductivite == 5000 :
+            print('Remarque : Votre mesure sera corrigée avec la température mesurée dans l\'échantillon pour plus de précision.')
+            input('Appuyez sur Entrée après avoir plongé le thermomètre dans la solution.')
+            lect = conductimeter.readline().decode().strip('\r\n').split(',')
+            T = float(lect[0])
+            conductivite = droite_5000(T)
+        elif conductivite == 1413 :
+            print('Remarque : Votre mesure sera corrigée avec la température mesurée dans l\'échantillon pour plus de précision.')
+            input('Appuyez sur Entrée après avoir plongé le thermomètre dans la solution.')
+            lect = conductimeter.readline().decode().strip('\r\n').split(',')
+            T = float(lect[0])
+            conductivite = droite_1413(T)
+        else : 
+            print('Remarque : Votre mesure ne pourra pas être corrigée selon la température.')
+            
+        input('\nAppuyez sur Entrée pour lancer la mesure.')
+        list_conductivite.append(conductivite)
+        list_tension_etalonnage = mesure_etalonnage(nbr_mesure_par_etalon)
+        ecart_type = np.std(list_tension_etalonnage)
+        print('L\'écart-type des mesures de l\'étalon est :', ecart_type,'V, un graphique s\'est aussi affiché.')
+        a = input('\nEst ce que la mesure est stable ? (y/n) : ')
+        moy_etalon = stabilite_mesure(a, list_tension_etalonnage, nbr_mesure_par_etalon)
+        print('La conductivité de votre étalon est de', conductivite, ' uS/cm avec une tension enregistrée de ', moy_etalon, 'V pour une température de', T, '°C.')
+        list_tension.append(float(moy_etalon))
+        print('Cette mesure est enregistrée, passez à la suite.\n')
+
+    plt.figure()
+    plt.plot(list_tension,list_conductivite, 'o', color = 'r')
+    plt.xlabel('Tension (V)')
+    plt.ylabel('Conductivité (us/cm)')
+    plt.axis([0, np.max(list_tension), 0, np.max(list_conductivite) +5])
+    plt.title('Courbe d\'étalonnage')
+    reg = np.polyfit(list_tension, list_conductivite, 1)
+    droite = np.poly1d(reg)
+    plt.plot(list_tension, droite[1] * np.array(list_tension) + droite[0])
+    plt.show()
+    
+    return droite
+
+
+
+
 
 
