73bb5262619141a4af6d61f4fef1cb56c16bdc9a
dfed <dfed@dfed-ipgp5>
2025-06-05 15:07:04 +0200

    Amélioration de l'interface utilisateur, et ajout du choix d'étalonnage par défaut pour l'utilisateur.

--- src/Programme conductimètre_V2.py
+++ src/Programme conductimètre_V2.py
@@ -34,59 +34,97 @@ if __name__ == '__main__':
 #
 ##################################################################
 
-    new_calib = False
-    continuer = True
-    calib3 = False
-    continuer_calib = True
-    default_model = default_calibration()
-    while continuer:
-        
-        x = input (interface)
-        
-        if x == '1':
-            
-            while continuer_calib :
-                interface_calib ="""
-    ===========================================================================
-    MENU CALIBRATION
-    ==========================================================================
-    Voulez - vous :
-    1 - Calibrer avec deux tampons (pH 7 et 4) ?
-    2 - Calibrer avec trois tampons (pH 7, 4 et 10) ?
-    3 - Calibrer à partir d'une calibration déjà existante dans le répertoire
-    4 - Quitter le menu calibration et retourner au menu principal
-    ===========================================================================
-    ? """
-                x_calib = input(interface_calib)
-                if x_calib == '1':
-                    calib3 = False
-                    errorbuffers_values = [0.01, 0.01]
-                    model = Calibration(buffers = [7, 4], n = 300, port_test = s)
-                    new_calib=True
+    """
+    Fonction d'accueil du conductimètre, lance le programme complet.'
 
-                elif x_calib == '2':
-                    calib3 = True
-                    errorbuffers_values = [0.01, 0.01, 0.01]
-                    model = Calibration(buffers = [7, 4, 10], n = 300, port_test = s)
-                    new_calib=True
+    Returns
+    -------
+    None.
 
-                elif x_calib == '3':
-                    model = Calibration_existante()
-                    new_calib = True
-
-                elif x_calib == '4':
-                    continuer_calib = False
-
-        elif x == '2':
-            y = input('Prêt à mesurer ?')
-            if new_calib :
-                measure(model, port_test = s)
-            else : 
-                measure(default_model, port_test = s)
-        elif x == "3":
-            graph()
-        elif x == '4':
-            continuer = False
+    """
     
-    print('Fin du programme')
+    #Initialisation des valeurs par défaut
+    nbr_mesure_par_echantillon = 100 
+    nbr_mesure_par_etalon = 200
+    interface_acceuil="""
+    ===========================================================================
+    ACCEUIL
+    ===========================================================================
+    Que voulez-vous faire ? :
+    1 - Etalonner le conductimètre 
+    2 - Mesurer la conductivité d'une solution
+    3 - Modifier les valeurs par défaut 
+    4 - Quitter
+    ===========================================================================
+    Votre réponse >>> ? """
+    
+    interface_calibration ="""
+    ===========================================================================
+    CALIBRATION
+    ===========================================================================
+    Que voulez-vous faire ? :
+    1 - Nouvel étalonnage
+    2 - Utiliser un étalonnage par défaut 
+    3 - Quitter
+    ===========================================================================
+    Votre réponse >>> ? """
+    
+    interface_type_etalonnage="""
+    ===========================================================================
+    Quel étalonnage par défaut 
+    ===========================================================================
+    1 - Utiliser l'étalonnage le plus récent
+    2 - Choisir un étalonnage particulier en fonction de son nombre d'é'
+    ===========================================================================
+    Votre réponse >>> ? """
+        
+    while(True) : 
+        reponse = input(interface_acceuil)
+        
+        if reponse == '1' : # Calibration
+            choix_calib=int(input(interface_calibration))
+            if choix_calib==1 : 
+                nbr_etalon = int(input("Combien d'étalons voulez-vous mesurer ? (au moins 1) : "))
+                droite_etalonnage= Etalonnage(nbr_etalon, nbr_mesure_par_etalon)
+                print('- Vous avez fini le calibrage.\n')
+            elif choix_calib==2:
+                type_etalonnage=int(input(interface_type_etalonnage))
+                if type_etalonnage==1:
+                    print("Utilisation du dernier étalonnage en date ") #A remplacer par le chargement du dernier coefficient a
+                elif type_etalonnage==2:
+                    nombre_etalons=int(input('Vous souhaitez charger un étalonnage avec combien de solutions étalons'))
+                    print('Par défaut nous chargerons l\'étalonnage le plus récent') # A modifier par l'etalonnage le plus récent 
+            elif choix_calib == '3' : # Arret du programme
+                print('- Merci, et bonne journée !')
+                break
+            
+            else :
+                print('Merci de répondre uniquement 1, 2 ou 3')
+                
+                 
+                    
+            
+        elif reponse == '2' : # Mesures
+            nbr_echantillon = int(input('Combien d\'échantillons voulez-vous mesurer ? : '))
+            Mesures(nbr_echantillon, droite, nbr_mesure_par_echantillon)
+            print('- Vous avez fini vos mesures.\n')
+            
+        elif reponse == '3' : # Modification des valeurs par défaut
+            choix_modif = input('\nQuelle valeur voulez-vous modifier ?\n1 : Nombre de valeurs par échantillon\n2 : Nombre de valeurs par étalon\nVotre réponse : ')
+            if choix_modif == '1' :
+                print('Actuellement, votre nombre de valeurs par échantillon est de : ', nbr_mesure_par_echantillon)
+                nbr_mesure_par_echantillon = int(input('Combien de mesures voulez-vous effectuer ? : '))
+            elif choix_modif == '2' : 
+                print('Actuellement, votre nombre de valeurs par étalon est de : ', nbr_mesure_par_etalon)
+                nbr_mesure_par_etalon = int(input('Combien de mesures voulez-vous effectuer pour chaque étalon ? : '))
+            else :
+                print('Veuillez répondre uniquement 1 ou 2')
+            
+        elif reponse == '4' : # Arret du programme
+            print('- Merci, et bonne journée !')
+            break
+        
+        else :
+            print('Merci de répondre uniquement 1, 2, 3 ou 4\n')
+    
 
